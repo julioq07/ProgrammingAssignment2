a178d1f275bc824f6ffb25f37072a6408aec8ef6
julioq07 <jquinones0709@gmail.com>
2020-06-05 21:06:54 -0500

    Solution for the assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function takes an object as a matrix or numeric class and 
+## save in the global enviorement its inverse form 
+ 
+## This function take the input metione above and create a 
+## list with the function for set the matrix, and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+          x <<- y
+          inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) inv <<- solve 
+  getinv <- function() inv
+  list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
 
-## Write a short comment describing this function
+## This function take the list of the set-get function for the matrix
+## and return the inverse value of our matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinv(inv)
+  inv
 }
